d8b73420dbffa3d23a5fb8613195f6cb1e488f75
GuimasGuimaraes <guilhermeguimaraes970@gmail.com>
2023-03-09 10:27:22 -0300

    first commit

---
+++ src/dominandoIdes.java
@@ -0,0 +1,11 @@
+
+public class dominandoIdes {
+
+	public static void main(String[] args) {
+		// criando repositorio no github
+
+		System.out.println("Primeiro repositorio no gitHub");
+
+	}
+
+}
